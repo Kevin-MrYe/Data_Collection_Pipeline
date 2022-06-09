f151cb867f0f28892a14d422be5bf3b4ffce59db
kevin <mrbinye@gmail.com>
2022-06-09 21:14:56 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(name='pipelines-metrics',
+       version='1.0',
+       packages=find_packages(),
+       install_requires=[
+           'selenium',
+           'tqdm',
+           'pandas',
+           'sqlalchemy',
+           'python-dotenv',
+           'boto3',
+        ])
